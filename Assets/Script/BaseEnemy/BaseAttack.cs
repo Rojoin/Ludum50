2a6aed27d115dceb1d718c6098ce8bf7afaff534
SlayerWx <maxi_cs_95@hotmail.com>
2022-04-02 14:46:23 -0300

    enemy basic attack

--- Assets/Script/BaseEnemy/BaseAttack.cs
+++ Assets/Script/BaseEnemy/BaseAttack.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class BaseAttack : MonoBehaviour
 {
     public Collider col;
+    public int damage;
     public float attackCooldown; // cooldown entre ataques
     public float timeWaitingToTryHit; // tiempo antes de intentar pegar el golpe
     public float timeTryingHit; // tiempo en el que el golpe puede hacer efecto
     public float timeRestToEnd; // tiempo despues de intentar golpear
     bool endAttack;
     bool canAttack;
+    GameObject target = null;
     void Start()
     {
         col.enabled = false;
@@ -27,14 +29,14 @@ public class BaseAttack : MonoBehaviour
     {
         if(other.gameObject.CompareTag("Player"))
         {
-
+            target = other.gameObject;
         }
     }
     private void OnTriggerExit(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-
+            if (target != null) target = null;
         }
     }
     IEnumerator Attacking()
@@ -48,6 +50,7 @@ public class BaseAttack : MonoBehaviour
         yield return new WaitForSeconds(timeTryingHit);
         Debug.Log("slashed");
         col.enabled = false;
+        if(target != null) target.GetComponent<PlayerHP>().TakeDamage(damage);
         yield return new WaitForSeconds(timeRestToEnd);
         Debug.Log("rest");
         endAttack = true;
