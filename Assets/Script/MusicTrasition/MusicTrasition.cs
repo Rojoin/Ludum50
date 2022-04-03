a6ab9e1f6cdb33fa052177c7d38a14a8772762f5
BarraSantiago <56093356+BarraSantiago@users.noreply.github.com>
2022-04-03 17:23:03 -0300

    UIRemakeBeta MusicBeta

--- Assets/Script/MusicTrasition/MusicTrasition.cs
+++ Assets/Script/MusicTrasition/MusicTrasition.cs
@@ -4,8 +4,36 @@ using UnityEngine;
 
 public class MusicTrasition : MonoBehaviour
 {
+    public PlayerHP playerHP;
+    public int lowHP = 30;
     private static MusicTrasition instance;
-    AudioSource myAudio;
+    private bool isDead;
+
+    private AudioSource[] allAudioSources;
+    public AudioSource myAudio;
+
+    public AudioClip playerDamaged;
+    public AudioClip playerNearDeath;
+    public AudioClip playerDeath;
+    public AudioClip playerMeleeHit1;
+    public AudioClip playerMeleeHit2;
+    public AudioClip playerMeleeAttack;
+    public AudioClip playerMeleeSwap;
+    public AudioClip playerPistolAttack;
+    public AudioClip playerPistolNoAmmo;
+    public AudioClip playerPickUpAmmo;
+    public AudioClip playerPickUpHeal;
+
+    public AudioClip enemyMeleeAttack;
+    public AudioClip enemyRangedAttack;
+    public AudioClip enemySpawn;
+    public AudioClip enemyDeath;
+    public AudioClip enemyGiantVoice;
+    public AudioClip enemySmallVoice;
+
+    public AudioClip backgroundMidGame;
+    public AudioClip backgroundEndGame;
+    //public AudioClip background
 
     void Start()
     {
@@ -13,6 +41,11 @@ public class MusicTrasition : MonoBehaviour
         myAudio.PlayDelayed(10.0f);
 
     }
+    private void Update()
+    {
+        LowHP();
+    }
+
     void awake()
     {
         if(instance == null)
@@ -23,6 +56,30 @@ public class MusicTrasition : MonoBehaviour
         else
         {
             Destroy(gameObject);
+        }
+    }
+    void LowHP()
+    {
+        if(playerHP.GetCurrentLifePoints() < lowHP)
+        {
+            myAudio.PlayOneShot(playerNearDeath);
+
+            if (playerHP.GetCurrentLifePoints()<=0 != isDead)
+            {
+                StopAllAudio();
+                myAudio.PlayOneShot(playerDeath);
+                isDead = true;
+            }
+        }
+
+    }
+
+    void StopAllAudio()
+    {
+        allAudioSources = FindObjectsOfType(typeof(AudioSource)) as AudioSource[];
+        foreach (AudioSource audioS in allAudioSources)
+        {
+            audioS.Stop();
         }
     }
 }
