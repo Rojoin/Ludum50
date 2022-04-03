d11166d2fd730ac2604b54631acbd69f1186062b
Rojoin <95532997+Rojoin@users.noreply.github.com>
2022-04-03 12:09:27 -0300

    Basic Distance

---
+++ Assets/Script/ShootingEnemy/Bullet.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    
+    Rigidbody rb;
+    Collider col;
+
+    bool collided = false;
+    Vector3 direccionoObjetiva;
+    [SerializeField] float projectileSpeed = 10;
+    [SerializeField]float velocity = 10; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb= GetComponent<Rigidbody>();
+     transform.LookAt(GetComponentInParent<DistanceAttack>().destination);
+     transform.parent=null;
+
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if(collider.gameObject.tag !="Player" && !collided)
+        {
+            collided = true;
+            gameObject.SetActive(false);
+        }
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = -transform.forward * velocity * Time.deltaTime;
+        
+    }
+}
---
+++ Assets/Script/ShootingEnemy/DistanceAttack.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistanceAttack : BaseAttack
+{
+   public  GameObject direction; 
+    public GameObject[] bullet;
+    public GameObject prefabBullet;
+    public Transform firePoint;
+    public Vector3 destination;
+
+    public GameObject a;
+    int balaIndex = 0;
+    
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        balaIndex = 0;
+        canAttack = true;
+        endAttack = false;
+        for (int i = 0; i< bullet.Length; i++)
+        {
+          
+          bullet[i] = Instantiate(prefabBullet,transform.position,Quaternion.identity,transform);
+
+        }
+    }
+
+    void ShootProjectile()
+    {
+        Debug.Log("Shoot Projectile");
+        balaIndex++;
+        Ray ray = new Ray(transform.position,Vector3.forward);
+        RaycastHit hit;
+        
+        Debug.Log("Atacando");
+        if(Physics.Raycast(ray,out hit))
+        {
+            destination = hit.point;
+            Instantiate(a,hit.point,Quaternion.identity);
+        }
+        else
+        {
+
+            destination = ray.GetPoint(1000);
+            Instantiate(a,destination,Quaternion.identity);
+        }
+        if(balaIndex >bullet.Length-1)
+        {
+            balaIndex = 0;
+        }
+       bullet[balaIndex].SetActive(true);
+
+
+        
+    }
+
+    public override IEnumerator Attacking()
+    {Debug.Log("Coroutine");
+        canAttack = false;
+        endAttack = false;
+        yield return new WaitForSeconds(timeWaitingToTryHit);
+        ShootProjectile();
+         endAttack = true;
+        yield return new WaitForSeconds(attackCooldown);
+         canAttack = true;
+    }
+    void InstatiateProjectile()
+    {
+        
+    }
+}
