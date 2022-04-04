dcc2a2dca43a5d83c5ab9b5924f49752af14a2c5
Rojoin <95532997+Rojoin@users.noreply.github.com>
2022-04-04 10:42:52 -0300

    Tutorial, bala y Sensibilidad

---
+++ Assets/Script/LoadAsync.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class LoadAsync : MonoBehaviour
+{
+    public Slider slider;
+    public GameObject bar;
+    public static string nextScene;
+    public static string fromScene;
+    public float waitToAppearButton;
+    public float waitToAppearBar;
+    bool buttonInput;
+    public GameObject button;
+    const float barProgressFix = 0.9f;
+    public static bool needCheckStart = true;
+    public void StartNextScene()
+    {
+        buttonInput = true;
+    }
+    public IEnumerator Start()
+    {
+        Time.timeScale = 1.0f;
+        yield return SceneManager.UnloadSceneAsync(fromScene);
+        Debug.Log(fromScene);
+        buttonInput = false;
+        slider.value = 0.0f;
+        bar.SetActive(true);
+        yield return new WaitForSecondsRealtime(waitToAppearBar);
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(nextScene);
+        Debug.Log(nextScene);
+        asyncLoad.allowSceneActivation = false;
+        while (asyncLoad.progress < barProgressFix)
+        {
+            slider.value = asyncLoad.progress;
+            Debug.Log("loop");
+            yield return null;
+        }
+        slider.value = 1.0f;
+        yield return new WaitForSecondsRealtime(waitToAppearButton);
+        bar.SetActive(false);
+        button.SetActive(true);
+        yield return null;
+        if (needCheckStart)
+        {
+            yield return new WaitUntil(() => buttonInput);
+        }
+        asyncLoad.allowSceneActivation = true;
+    }
+}
